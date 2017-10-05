d1f3180f4664c6d0affc2ed0bd49acb2a937342a
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-10-05 11:45:40 +0200

    Create a react-native component

---
+++ reactnative/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Squadteam 45!</Text>
+      </View>
+    );
+  }
+}
