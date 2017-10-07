63b5a3f0bf02cc4b23473964be3a37a418d8bcbd
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-10-07 15:09:25 +0200

    Create form for setting name

---
+++ src/Components/SetName.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Button, Form, Grid, Header, Icon, Message, Segment } from 'semantic-ui-react'
+
+export class SetName extends React.Component{
+  constructor(){
+    super();
+    this.state= {
+      name:""
+    }
+  }
+
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+  handleSubmit = () => {
+    this.props.setName(this.state.name)
+  }
+
+  render(){
+    const name = this.state.name
+    return (
+
+      <div className='login-form'>
+        <style>{`
+          body > div,
+          body > div > div,
+          body > div > div > div.login-form {
+            height: 100%;
+          }
+        `}</style>
+        <Grid
+          textAlign='center'
+          style={{ height: '100%' }}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='teal' textAlign='center'>
+              <Icon name='calendar' circular />
+              {' '}Name Plz
+            </Header>
+            <Form size='large' onSubmit={this.handleSubmit.bind(this)}>
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon='user'
+                  iconPosition='left'
+                  placeholder='Name'
+                  name='name'
+                  value={name}
+                  onChange={this.handleChange}
+                />
+                <Button color='teal' fluid size='large' type='submit'>Register</Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default SetName
