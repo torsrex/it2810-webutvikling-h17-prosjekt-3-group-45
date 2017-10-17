ed344e3ef253c657ad75a1e3a8ba3c47fa723504
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-10-17 12:58:20 +0200

    Fix test for css

--- react/__tests__/app.test.jsx
+++ react/__tests__/app.test.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src//App';
+
 describe('App', () => {
   it('should be able to run tests', () => {
     expect(1 + 2).toEqual(3);
+  });
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
   });
 });
