8129e3e592b10ff8b65b81f97cbd5573c8b3bc2a
Andreas Berger <Andreas.Berger.ab@gmail.com>
2017-10-18 14:27:41 +0200

    FIX2.0

--- react/src/Components/Contacts/Contacts.jsx
+++ react/src/Components/Contacts/Contacts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Contact } from './ContactItem';
-import { AddContact } from './AddContact';
+import AddContact from './AddContact';
 
 export default class Contacts extends React.Component{
 
